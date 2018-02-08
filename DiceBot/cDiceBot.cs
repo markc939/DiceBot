cbff04fdceaf5c11b9cb2ecec1a09e68099a9ed7
markc939 <machilds@gmail.com>
2018-02-08 16:11:36 +0000

    fix

--- DiceBot/cDiceBot.cs
+++ DiceBot/cDiceBot.cs
@@ -1098,8 +1098,12 @@ namespace DiceBot
                     if (!StatsWindows.IsDisposed)
                     {
 
-                        StatsWindows.lblLoseStreak.Text = WorstStreak.ToString() + ((Lua["WorstSimulatedLossStreak"] != null) ? " / Sim: " + Lua["WorstSimulatedLossStreak"] : "");
-                                      
+                        StatsWindows.lblLoseStreak.Text = WorstStreak.ToString();
+                        if(Lua["WorstSimulatedLossStreak"] != null)
+                        {
+                            StatsWindows.lblLoseStreak.Text += " / Sim: " + Lua["WorstSimulatedLossStreak"];
+                        }
+              
                         lblLosses2.Text = StatsWindows.lblLosses.Text = Losses.ToString();
 
                         // MARKC
